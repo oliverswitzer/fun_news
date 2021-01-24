e213330618ec885da2ed7ee3d00c5a71054135b0
Oliver Switzer <oliverswitzer@gmail.com>
2021-01-24 14:17:36 -0500

    Cleanup: move shape generation out to a shape factory. Some variable renames

--- assets/js/fun-zoom.js
+++ assets/js/fun-zoom.js
@@ -1,15 +1,18 @@
 import Matter from "matter-js";
 import _shuffle from 'lodash/shuffle';
 import _random from 'lodash/random';
+import {ShapeFactory} from "./shape-factory";
 
-const Engine = Matter.Engine,
-  Render = Matter.Render,
-  World = Matter.World,
-  Runner = Matter.Runner,
+const Constraint = Matter.Constraint,
+  Engine = Matter.Engine,
   Events = Matter.Events,
-  Bodies = Matter.Bodies;
+  Mouse = Matter.Mouse,
+  MouseConstraint = Matter.MouseConstraint,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  World = Matter.World;
 
-const BIAS_COLOR_MAP = {
+const POLITICAL_BIAS_COLOR_MAP = {
   "Left": "#2f66e9",
   "Lean Left": "#4977ea",
   "Center": "#771cb6",
@@ -17,6 +20,8 @@ const BIAS_COLOR_MAP = {
   "Lean Right": "#ea7e7e",
   "Right": "#ea1919"
 }
+
+const shapeFactory = new ShapeFactory(screenWidth, screenHeight)
 
 export async function render(container) {
   const engine = Engine.create();
@@ -43,62 +48,16 @@ export async function render(container) {
   Runner.run(runner, engine);
 
   addMouse(engine, world, render);
-  const groundWidth = screenWidth();
-  const groundThickness = 60;
-  const projectile = createCircle(80, screenHeight() - groundThickness, groundThickness);
-  const ground = Bodies.rectangle(groundWidth/2, screenHeight() - groundThickness/2, screenWidth(), 60, {isStatic: true});
-  const leftWall = Bodies.rectangle(-screenWidth()/2 + groundThickness, screenHeight()/2, groundWidth, screenHeight(), {isStatic: true});
-  World.add(world, [projectile, ground,leftWall]);
 
-  spawnNewsBlocks(render, world)
+  const projectile = shapeFactory.createCircle(80, screenHeight() - 60, 60);
+  const walls = shapeFactory.createWalls(60);
+  World.add(world, [projectile].concat(walls));
 
-  let lastArticle = null;
-  const collisionStart = function(event) {
-    const collision = event.pairs[0];
-    if(collision.bodyB.label !== "Circle Body") { return; } // then the ball is colliding with the ground
-    if(collision.bodyA.href) { return; } // bodyA should always be the users particle. Collisions between news articles are not allowed
+  await spawnNewsBlocks(render, world)
+  addCollisionListener(world, engine);
+  addDocumentVisibilityListener(world, engine, render);
 
-    const particle = collision.bodyA;
-    const article = collision.bodyB;
-
-    if(!article.tied) {
-      const constraint = Matter.Constraint.create({
-        bodyA: lastArticle ? lastArticle : particle,
-        bodyB: article,
-        pointB: { x: 0, y: 0},
-        pointA: { x: 0, y: 0},
-        stiffness: 1
-      })
-      World.add(world, constraint)
-      Matter.Body.setStatic(article, false);
-      article.tied = true;
-      article.isSensor = true;
-
-      lastArticle = article;
-    }
-
-    if (article.href && !article.lookedAt) {
-      article.lookedAt = true;
-      window.open(article.href, "_blank")
-    }
-  };
-
-  document.addEventListener("visibilitychange", function() {
-    const currentConstraints = world.constraints.map(c => c.label)
-
-    if (document.visibilityState === 'visible' && !currentConstraints.includes("Mouse Constraint")) {
-      addMouse(engine, world, render)
-    } else {
-      removeMouse(world);
-    }
-  });
-
-  Events.on(engine, 'collisionStart', collisionStart);
   Engine.run(engine);
-}
-
-function createCircle(x, y, radius, options) {
-  return Bodies.circle(x, y, radius*scaleFactor(), options);
 }
 
 function removeMouse(world) {
@@ -106,8 +65,8 @@ function removeMouse(world) {
   World.remove(world, mouseConstraint)
 }
 function addMouse(engine, world, render) {
-  const mouse = Matter.Mouse.create(render.canvas),
-    mouseConstraint = Matter.MouseConstraint.create(engine, {
+  const mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
       constraint: {
         stiffness: 0.2,
@@ -116,22 +75,21 @@ function addMouse(engine, world, render) {
         }
       }
     });
-  Matter.World.add(world, mouseConstraint);
+  World.add(world, mouseConstraint);
   render.mouse = mouse;
-
 }
 
 async function spawnNewsBlocks(render, world) {
   const newsArticles = await fetch('/api/news.json').then(res => res.json())
   const newsBlocks = _shuffle(newsArticles.data).splice(0,30).map(article => {
-    const newsRect = createCircle(
+    const newsRect = shapeFactory.createCircle(
       _random(30, render.options.width - 30),
       _random(30, render.options.height - 200),
       30,
       {
         isStatic: true,
         render: {
-          fillStyle: BIAS_COLOR_MAP[article.bias]
+          fillStyle: POLITICAL_BIAS_COLOR_MAP[article.bias]
         }
       }
     );
@@ -151,6 +109,50 @@ function screenHeight() {
   return document.body.clientHeight;
 }
 
-function scaleFactor() {
-  return (screenWidth() > 1200 ? 1200 : screenWidth()) / 1200
+function addDocumentVisibilityListener(world, engine, render) {
+  document.addEventListener("visibilitychange", function () {
+    const currentConstraints = world.constraints.map(c => c.label)
+
+    if (document.visibilityState === 'visible' && !currentConstraints.includes("Mouse Constraint")) {
+      addMouse(engine, world, render)
+    } else {
+      removeMouse(world);
+    }
+  });
+}
+
+function addCollisionListener(world, engine) {
+  let lastArticleOpened = null;
+
+  const collisionStart = function (event) {
+    const collision = event.pairs[0];
+
+    if (collision.bodyB.label !== "Circle Body") {return;} // then the ball is colliding with the ground
+    if (collision.bodyA.href) {return;} // bodyA should always be the user's particle. Collisions between news articles are not allowed
+
+    const particle = collision.bodyA;
+    const article = collision.bodyB;
+
+    if (!article.tiedToPlayer) {
+      const constraint = Constraint.create({
+        bodyA: lastArticleOpened ? lastArticleOpened : particle,
+        bodyB: article,
+        pointB: {x: 0, y: 0},
+        pointA: {x: 0, y: 0},
+        stiffness: 1
+      })
+      World.add(world, constraint)
+      Matter.Body.setStatic(article, false);
+      article.tiedToPlayer = true;
+      article.isSensor = true;
+
+      lastArticleOpened = article;
+    }
+
+    if (article.href && !article.previouslyLookedAt) {
+      article.previouslyLookedAt = true;
+      window.open(article.href, "_blank")
+    }
+  };
+  Events.on(engine, 'collisionStart', collisionStart);
 }
---
+++ assets/js/shape-factory.js
@@ -0,0 +1,28 @@
+import Matter from "matter-js";
+
+const Bodies = Matter.Bodies;
+
+export class ShapeFactory {
+  constructor(screenWidthFun, screenHeightFun) {
+    this.screenWidthFun = screenWidthFun;
+    this.screenHeightFun = screenHeightFun;
+  }
+
+  createCircle(x, y, radius, options) {
+    const scaleFactor = (this.screenWidthFun() > 1200 ? 1200 : this.screenWidthFun()) / 1200;
+
+    return Bodies.circle(x, y, radius * scaleFactor, options);
+  }
+
+  createRectangle(x, y, w, h, options) {
+    return Bodies.rectangle(x, y, w, h, options)
+  }
+
+  createWalls(wallThickness) {
+    const ground = this.createRectangle(this.screenWidthFun() / 2, this.screenHeightFun() - wallThickness / 2, this.screenWidthFun(), wallThickness, {isStatic: true});
+    const ceiling = this.createRectangle(this.screenWidthFun() / 2, 0, this.screenWidthFun(), wallThickness, {isStatic: true});
+    const leftWall = this.createRectangle(0, this.screenHeightFun() / 2, wallThickness, this.screenHeightFun(), {isStatic: true});
+    const rightWall = this.createRectangle(this.screenWidthFun(), this.screenHeightFun() / 2, wallThickness, this.screenHeightFun(), {isStatic: true});
+    return [leftWall, ground, rightWall, ceiling]
+  }
+}
